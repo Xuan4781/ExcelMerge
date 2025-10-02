8ffa04306d3fa6ff8495ac38f56f02e0ab32b620
Angela Gao <Xuan4781@users.noreply.github.com>
2025-10-02 16:59:36 -0400

    Combined house and street into Address, renamed cols, added edge cases and merged

--- emerge.py
+++ emerge.py
@@ -47,3 +47,51 @@ parking = parking.rename(columns={
 merged = primary.merge(parking, on="BIN", how="left")
 merged = merged.merge(local88, on="BIN", how="left")
 merged = merged.merge(local97, on="BIN", how="left")
+
+
+
+# Creating Address from house_NO and Streename + cycle & subcycle
+merged["Address"] = merged["HOUSE_NO"].astype(str) + " " + merged["STREET_NAME"]
+merged["FISP Cycle"] = merged["CYCLE"].astype(str) + " / " + merged["SUBCYCLE"].astype(str)
+
+# renamed primary 
+merged = merged.rename(columns={
+    "CURRENT_STATUS": "FISP Compliance Status",
+    "PRIOR_STATUS": "FISP Last Filing Status",
+    "PRIOR_CYCLE_FILING_DATE": "FISP Cycle Filing Window",
+    "COMMENTS": "FISP Notes / Budget Request",
+    "# Floors": "M Floors",
+    "Approx. SF": "Approx Sq Ft",
+    "OWNER_NAME": "Building Owner/Manager"
+})
+
+
+
+
+# For remaining headers, have empty columns
+for col in [
+    "LL126 Cycle", "LL126 Filing Window", "LL126 Parapet Compliance Status", "LL126 Parapet Notes",
+    "LL84 Compliance Status", "LL84 Filing Due", "LL84 Next Steps",
+    "LL87 Compliance Status", "LL87 Filing Due", "LL87 Compliance Year", "LL87 Next Steps",
+    "Contact Email", "Contact Phone"
+]:
+    if col not in merged.columns:
+        merged[col] = ""
+
+# Reordering headers
+final_headers = [
+    "Address","Building Owner/Manager","Use Type","Block","BIN","Borough","Year Built",
+    "M Floors","Approx Sq Ft","Landmark","Parking Garage","FISP Compliance Status",
+    "FISP Cycle","FISP Last Filing Status","FISP Cycle Filing Window","FISP Next Steps","FISP Notes / Budget Request",
+    "LL126 Compliance Status","LL126 Cycle","LL126 Previous Filing Status","LL126 SREM Recommended Date",
+    "LL126 Filing Window","LL126 Next Steps","LL126 Notes / Budget Request","LL126 Parapet Compliance Status",
+    "LL126 Parapet Notes","LL84 Compliance Status","LL84 Filing Due","LL84 Next Steps","LL87 Compliance Status",
+    "LL87 Filing Due","LL87 Compliance Year","LL87 Next Steps","LL88 Compliance Status","LL88 Filing Due",
+    "LL88 Notes","LL97 Compliance Status","LL97 Filing Due","LL97 Next Steps","Contact Email","Contact Phone"
+]
+
+merged = merged[final_headers]
+
+
+merged.to_excel("final_merged.xlsx", index=False)
+
